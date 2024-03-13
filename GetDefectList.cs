f64f0c67465167ded9392732651859f4ac91a77b
Shakiba Kouchakpour <shakiba.kouchakpour@cgi.com>
2024-03-13 08:20:42 +0100

    Adding a function to get the defect list

---
+++ GetDefectList.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using AzureFunction_RepRepair.Models;
+using Newtonsoft.Json;
+
+namespace AzureFunction_RepRepair
+{
+    public static class GetDefectList
+    {
+        [Function("GetDefectList")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetDefectList")] HttpRequestData req,
+            [SqlInput("SELECT * FROM [Scan].[DefectList]",
+            "ConnectionString")] IEnumerable<DefectList> defects)
+        {
+            var result = JsonConvert.SerializeObject(defects);
+            if (result == null || !result.Any())
+            {
+                return new NoContentResult();
+            }
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ Models/DefectList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunction_RepRepair.Models
+{
+    public class DefectList
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+    }
+}
